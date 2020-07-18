a3d3005d9387a4da1da3ddb179170c31e6666abb
albertovpd <a.vargas.pina@gmail.com>
2020-07-18 22:04:09 +0200

    fixing vscode commands

--- linux_programs.sh
+++ linux_programs.sh
@@ -1,9 +1,15 @@
-sudo apt-get install chromium
-sudo apt install git-all
+sudo apt install chromium-browser
 sudo apt-get install krita
 sudo apt-get install usb-creator-gtk # Startup Disk Creator
-sudo apt-get install Spotify
-sudo apt-get install Terminator
-sudo apt-get install ubunto-Tweaks
-sudo apt-get install code # visual studio code
-    
+sudo snap install spotify
+sudo apt-get install terminator
+
+sudo add-apt-repository ppa:trebelnik-stefina/ubuntu-tweak
+sudo apt update
+sudo apt install ubuntu-tweak 
+
+sudo apt install software-properties-common apt-transport-https wget # visual studio code    
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code 

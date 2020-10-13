988fa2d9378dda9396d7739095a5194407e18298
albertovpd <a.vargas.pina@gmail.com>
2020-10-13 10:24:46 +0200

    upadte added, obs done

--- all_libraries.sh
+++ all_libraries.sh
@@ -3,20 +3,24 @@
 # --------------------------------
 # create environment: pass env name as first positional
 conda create --name $1 -y
+# jupyter-lab
+conda install --name $1 -c conda-forge jupyterlab -y
 
 # --------------------------------
-
 #   LIBRARIES
 # --------------------------------
 
 # basics data science
-conda install -c conda-forge jupyterlab
 conda install --name $1 -c anaconda pandas -y
 conda install --name $1 -c conda-forge matplotlib -y
 conda install --name $1 -c anaconda numpy -y
 conda install --name $1 -c anaconda seaborn -y
-#conda install --name $1 -c anaconda scikit-learn
-#conda install --name $1 -c anaconda scipy
+conda install --name $1 -c anaconda scikit-learn
+conda install --name $1 -c anaconda scipy
+
+
+#cgsfs
+#conda install -c conda-forge gcsfs
 
 # --------------------------------
 
--- linux_programs.sh
+++ linux_programs.sh
@@ -1,13 +1,23 @@
+sudo apt-get update
+
 sudo apt install chromium-browser
 sudo apt-get install krita
-sudo apt-get install usb-creator-gtk # Startup Disk Creator
+sudo apt install obs-studio
 sudo snap install spotify
-sudo apt-get install terminator
-sudo apt-get install psensor 
 
-sudo add-apt-repository ppa:trebelnik-stefina/ubuntu-tweak
-sudo apt update
-sudo apt install ubuntu-tweak 
+
+sudo apt-get install usb-creator-gtk # Startup Disk Creator
+sudo apt-get install terminator
+sudo apt-get install psensor
+sudo apt install curl 
+
+
+sudo add-apt-repository universe
+sudo apt install gnome-tweak-tool
+#sudo add-apt-repository ppa:trebelnik-stefina/ubuntu-tweak
+#sudo apt update
+#sudo apt install ubuntu-tweak 
+
 
 sudo apt install software-properties-common apt-transport-https wget # visual studio code    
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
